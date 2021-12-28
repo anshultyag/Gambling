f218b89a93d9fc9f6ef8b498960b48412ddc8851
anshultyag <you@example.com>
2021-12-28 16:29:25 +0530

    Added: if won or lost is 50%

--- src/UC_01.java
+++ src/UC_01.java
@@ -5,14 +5,16 @@ public class UC_01 {
         int stake = 100;
         int bet = 1;
         System.out.println("Gambling world,Everyday stake is:" + stake + "  Bet for every game is:" + bet);
-        double random = Math.random();
-        if (random>0.5){
-            int total = stake-bet;
-            System.out.println("Lost amount is:" + stake + "and stake amount is" + total);
-        }else{
-            int total = stake-bet;
-            System.out.println("Win amount is :" + bet + "And stake amount is" + total);
-        }
+        while (stake != 50 && stake != 150) {
+            double random = Math.random();
+            if (random < 0.5) {
+                stake = stake + bet;
+                System.out.println("Lost amount is: " + bet + "  and stake amount is: " + stake);
+            } else {
+               stake = stake - bet;
+                System.out.println("Win amount is :" + bet + " And stake amount is: " + stake);
+            }
 
+        }
     }
 }
