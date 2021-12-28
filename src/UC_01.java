938394f04bc5cd96dbbaaad22f1dc92f0c92edb5
anshultyag <you@example.com>
2021-12-28 15:42:44 +0530

    Added: Stake or bet

---
+++ src/UC_01.java
@@ -0,0 +1,10 @@
+public class UC_01 {
+
+    public static void main(String[] args) {
+
+        int stake = 100;
+        int bet = 1;
+        System.out.println("Gambling world,Everyday stake is:" + stake + "Bet for every game is:" + bet);
+        
+    }
+}
