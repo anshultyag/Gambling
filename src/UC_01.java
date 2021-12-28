19ab4d7bebb729702ec3f59f8a129d05988c1120
anshultyag <you@example.com>
2021-12-28 16:05:10 +0530

    Added: loss or win amount

--- src/UC_01.java
+++ src/UC_01.java
@@ -4,7 +4,15 @@ public class UC_01 {
 
         int stake = 100;
         int bet = 1;
-        System.out.println("Gambling world,Everyday stake is:" + stake + "Bet for every game is:" + bet);
-        
+        System.out.println("Gambling world,Everyday stake is:" + stake + "  Bet for every game is:" + bet);
+        double random = Math.random();
+        if (random>0.5){
+            int total = stake-bet;
+            System.out.println("Lost amount is:" + stake + "and stake amount is" + total);
+        }else{
+            int total = stake-bet;
+            System.out.println("Win amount is :" + bet + "And stake amount is" + total);
+        }
+
     }
 }
